ce6965ac431d6a0f811a442bcf6f69a0d6206e8f
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2024-03-23 20:27:27 +0800

    Init session keyring for samsung
    Patch provided by user 'rhjdvsgsgks' on github.
    
    Fixes:
    https://github.com/tiann/KernelSU/issues/1249
    https://github.com/tiann/KernelSU/issues/1346

--- kernel/kernel_compat.c
+++ kernel/kernel_compat.c
@@ -10,7 +10,7 @@
 #include "klog.h" // IWYU pragma: keep
 #include "kernel_compat.h" // Add check Huawei Device
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI)
+#if 1
 #include "linux/key.h"
 #include "linux/errno.h"
 #include "linux/cred.h"
@@ -89,7 +89,7 @@ int ksu_access_ok(const void *addr, unsigned long size) {
 
 struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI)
+#if 1
 	if (init_session_keyring != NULL && !current_cred()->session_keyring &&
 	    (current->flags & PF_WQ_WORKER)) {
 		pr_info("installing init session keyring for older kernel\n");
