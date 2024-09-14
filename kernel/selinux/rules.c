35f527260dde6da401cacf7b143fa631951d2787
walternewtz <77871609+walternewtz@users.noreply.github.com>
2024-09-14 22:39:12 +0530

    Update rules.c

--- kernel/selinux/rules.c
+++ kernel/selinux/rules.c
@@ -69,6 +69,8 @@ void apply_kernelsu_rules()
 	// we need to save allowlist in /data/adb/ksu
 	ksu_allow(db, "kernel", "adb_data_file", "dir", ALL);
 	ksu_allow(db, "kernel", "adb_data_file", "file", ALL);
+	ksu_allow(db, "kernel", "cache_file", "dir", ALL);
+        ksu_allow(db, "kernel", "cache_file", "file", ALL);
 	// we need to search /data/app
 	ksu_allow(db, "kernel", "apk_data_file", "file", "open");
 	ksu_allow(db, "kernel", "apk_data_file", "dir", "open");
